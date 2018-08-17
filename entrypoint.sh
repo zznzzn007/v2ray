65571512c12701ecbe20825066622092adcfdcf2
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-08-17 20:23:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
-cd /v2raybin
-echo -e "$CONFIG_JSON" > config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
-fi
+#cd /v2raybin
+#echo -e "$CONFIG_JSON" > config.json
+#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+#  echo -e "$CERT_PEM" > cert.pem
+#  echo -e "$KEY_PEM"  > key.pem
+#fi
+cd /v2ray
+wget http://storage.googleapis.com/v2ray-docker/v2ray 
+wget http://storage.googleapis.com/v2ray-docker/v2ctl
+wget http://storage.googleapis.com/v2ray-docker/geoip.dat
+wget http://storage.googleapis.com/v2ray-docker/geosite.dat
+
+chmod +x v2ray v2ctl
+sed -i "s/your_uuid/$UUID/g" config.json
 ./v2ray
