05144e4fe70b84114fbe1d3a53566d49d3f53b88
zznzzn007 <15836244@qq.com>
2018-10-08 18:34:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,6 @@
 cd /v2ray
 wget -O v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v3.46.4/v2ray-linux-64.zip
 unzip v2ray.zip 
-mv /v2ray/v2ray-v3.46.4-linux-64/v2ray .
-mv /v2ray/v2ray-v3.46.4-linux-64/v2ctl .
-mv /v2ray/v2ray-v3.46.4-linux-64/geoip.dat .
-mv /v2ray/v2ray-v3.46.4-linux-64/geosite.dat .
-
 chmod +x v2ray v2ctl
-sed -i "s/b41da9a6-d88b-49b1-852b-5fbe3f14a83b/$UUID/g" config.json
+sed -i "s/your_uuid/$UUID/g" config.json
 ./v2ray
